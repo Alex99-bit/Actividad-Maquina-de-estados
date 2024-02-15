ca9f1f9bb31a2ace623024955ecb22c88282c9f3
Carlos Alejandro <alexco_99@hotmail.com>
2024-02-15 16:27:52 -0600

    Update VacaMecha.cs

--- Assets/Scripts/VacaMecha.cs
+++ Assets/Scripts/VacaMecha.cs
@@ -8,7 +8,7 @@ public class VacaMecha : MonoBehaviour
     public CowStates currentCowState;
 
     [SerializeField]
-    int hambre, resitencia, lactancia, estres;
+    float hambre, resitencia, lactancia, estres;
 
     float timeDriver, timeLapse;
 
@@ -51,11 +51,68 @@ public class VacaMecha : MonoBehaviour
     {
         Debug.Log("Hambre: " + hambre + " Resitencia: " + resitencia + " Lactancia: " + lactancia + " Estres: " + estres);
 
+        // hambre, resitencia, lactancia, estres
+        // Aqui se asegura de que no sobre pasen los limites
+        if (lactancia >= 100)
+        {
+            lactancia = 100;
+        }
+
+        if (hambre >= 100)
+        {
+            hambre = 100;
+        }
+
+        if (resitencia >= 100)
+        {
+            resitencia = 100;
+        }
+
+        if (estres >= 100)
+        {
+            estres = 100;
+        }
+
+        // Y aqui que no sean mas bajos
+        if (lactancia <= 0)
+        {
+            lactancia = 0;
+        }
+
+        if (hambre <= 0)
+        {
+            hambre = 0;
+        }
+
+        if (resitencia <= 0)
+        {
+            resitencia = 0;
+        }
+
+        if (estres <= 0)
+        {
+            estres = 0;
+        }
+
         // Aqui se evalua el estado en caso de que se tenga que ejecutar de forma variable o constante algo dependiendo del estado
         switch (currentCowState)
         {
             case CowStates.pastar:
+                if (timeDriver >= timeLapse)
+                {
+                    timeDriver = 0;
+                    hambre += 7;
+                    estres -= 0.3f;
 
+                    if (hambre > 77)
+                    {
+                        lactancia += 3;
+                    }
+                    else if (hambre > 40)
+                    {
+                        lactancia++;
+                    }
+                }
                 break;
             case CowStates.ordenia:
 
@@ -97,10 +154,12 @@ public class VacaMecha : MonoBehaviour
     // Funcion que se llama en caso especial para cambiar de estado y ejecutar algo una sola vez
     public void SetNewState(CowStates newState)
     {
+        timeDriver = 0;
+
         switch (newState)
         {
             case CowStates.pastar:
-
+                
                 break;
             case CowStates.ordenia:
 
